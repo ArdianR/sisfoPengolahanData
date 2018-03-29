c05875a74df28980fd67357a47f647cf73351e7b
edhodev <riidho.lfc@gmail.com>
2018-03-29 12:02:06 +0800

    update rekap2

--- API/presensiKeluar.php
+++ API/presensiKeluar.php
@@ -9,24 +9,25 @@ $result = mysqli_query($con, "select id from karyawan where nama='$nama'");
 if(mysqli_num_rows($result) > 0){
     while($id = mysqli_fetch_assoc($result)){
         $selectID = $id['id'];
-        $query = "update daftarKehadiran set waktuPulang='$waktuPulang' where id='$selectID' AND waktuPulang=null";
-        if(mysqli_query($con, $query)===TRUE){
+        $query = "update daftarKehadiran set waktuPulang='$waktuPulang' where id='$selectID'";
+        if(mysqli_query($con, $query)){
             mysqli_query($con, "update daftarKehadiran set jumlahJamKerja=TIMEDIFF(waktuPulang, waktuMasuk) where id='$selectID' AND tanggal='$tanggal'"); //update data (Jumlah Jam Kerja)
-            $cariTanggal = mysqli_query($con, "SELECT date_add(tanggal, interval 30 day) as batas from daftarKehadiran"); // Menentukan Batas Tanggal Gaji
+            $cariTanggal = mysqli_query($con, "SELECT tanggal, date_add(tanggal, interval 2 day) as batas from daftarKehadiran where id='$selectID' AND tanggal='$tanggal'"); // Menentukan Batas Tanggal Gaji
             $batasTanggal = mysqli_fetch_assoc($cariTanggal);
-			$valid = mysqli_query($con, "SELECT * from rekapKehadiran where id='$selectID' and batasTanggal='$batasTanggal[batas]'");
+
+			$valid = mysqli_query($con, "SELECT * from rekapKehadiran where id='$selectID' AND batasTanggal >= '$batasTanggal[tanggal]'");
+
 			$result2	   = mysqli_query($con, "SELECT * from daftarKehadiran where id='$selectID' AND tanggal='$tanggal'");
 			$total   	   = mysqli_fetch_assoc($result2);
 			$totalJamKerja = $total['jumlahJamKerja'];	
 			if(mysqli_num_rows($valid) > 0 ){
 				mysqli_query($con, "UPDATE rekapKehadiran SET jumlahKehadiran=jumlahKehadiran+1,totalJamKerja=ADDTIME(totalJamKerja, '$totalJamKerja') where id='$selectID'");	
 			}
-			else {
-				
-				
+			else {				
 				mysqli_query($con, "INSERT INTO rekapKehadiran (id, jumlahKehadiran, totalJamKerja, Bulan, Tahun, batasTanggal) values($selectID, 1, '$totalJamKerja','Maret', '2018', '$batasTanggal[batas]')");
 				}
 			}	
 		}
     }
 ?>
+	
